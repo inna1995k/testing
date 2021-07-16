6787d43c9b377675a958986ad5171f6227fb856f
inna <basketboll95@yandex.ru>
2021-07-16 10:39:45 +0300

    rel2.0

--- src/com/company/Develop.java
+++ src/com/company/Develop.java
@@ -5,5 +5,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class Develop {
     public static void main(String[] args) {
         System.out.println("Dev");
+        System.out.println("Merged Dev");
     }
 }
