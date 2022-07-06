7578ea48c65c2744f6aaa64305abe92ea8830a30
inna <basketboll95@yandex.ru>
2022-07-06 13:10:58 +0300

    add new class after reset

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,13 @@
 package com.company;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("alex");
+        System.out.println("Cherry pick");
     }
 }
